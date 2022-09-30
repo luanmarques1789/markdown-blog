005f0379adef358ce351be38302f0524b2e72bd8
Luan Marques <luanmarques1789@gmail.com>
2022-09-29 21:43:12 -0300

    Card component for posts/articles

---
+++ components/PostCard.jsx
@@ -0,0 +1,27 @@
+import Image from './Image';
+import Link from './Link';
+
+export default function PostCard({ index, slug, title, excerpt, image }) {
+	return (
+		<div
+			style={{
+				marginBottom: '5px',
+				display: 'block',
+				backgroundColor: 'red',
+				width: '100%',
+				height: '100%',
+			}}
+		>
+			<Link href={`/posts/${slug}`}>
+				<Image
+					src={image.src}
+					alt={image.description}
+					width={200}
+					height={200}
+				/>
+				<h2>{title}</h2>
+			</Link>
+			<p>{excerpt}</p>
+		</div>
+	);
+}
