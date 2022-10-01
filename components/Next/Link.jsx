4bf937496c9d0a12cda4c9d6204cbb6b3cb45a3f
Luan Marques <luanmarques1789@gmail.com>
2022-10-01 17:20:49 -0300

    Adding value 'noopener' to external links for performance and security issues

--- components/Next/Link.jsx
+++ components/Next/Link.jsx
@@ -1,9 +1,14 @@
 import NextLink from 'next/link';
 
-export default function Link(props) {
+export default function Link({ href, target, children }) {
 	return (
-		<NextLink href={props.href}>
-			<a target={props.target ? props.target : '_self'}>{props.children}</a>
+		<NextLink href={href}>
+			<a
+				target={target ? target : '_self'}
+				rel={target === '_blank' ? 'noopener' : ''}
+			>
+				{children}
+			</a>
 		</NextLink>
 	);
 }
